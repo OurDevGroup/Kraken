134539ee285f3029283dd10e49e15e98d4f7d1b3
U-CORP\RIFER <RIFER@SCL77724HSMDFK.corp.msi.michaels.com>
2015-08-14 11:56:43 -0500

    turned on upload

--- bin/dwdav.sh
+++ bin/dwdav.sh
@@ -51,8 +51,6 @@ dw_upload_build() {
     
 	dwtarget="https://$demandwareServer/on/demandware.servlet/webdav/Sites/Cartridges/${dwbuild}"
 	
-	exit 1
-	
 	if [ $requiresClientCertificate == true ]; then
         echo
         echo "Uploading with certificate authentication..."
