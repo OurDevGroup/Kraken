2fa4067da072ea9c9c57470cdb3c44f12951258c
Ryan Rife <rwrife@gmail.com>
2016-02-03 10:18:04 -0600

    Revert "merge conflict"
    This reverts commit be26617188e6ca9b1b5fe948a947af691c6ca258.

--- bin/func.sh
+++ bin/func.sh
@@ -6,7 +6,7 @@ downer() {
     echo $1 | tr "[A-Z]" "[a-z]"
 }
 
-_md5() { 
+_md5() {
   md5=$(command -v md5);
   md5sum=$(command -v md5sum);
   if [ "$md5" > /dev/null ]; then
@@ -15,7 +15,7 @@ _md5() {
     echo "$1" | md5sum | awk '{print $1}'
   else
     echo "$1"
-  fi   
+  fi
 }
 
 inc_build_number() {
@@ -24,28 +24,22 @@ inc_build_number() {
 	write_conf "deploy" "build" $build
 }
 
-zip_cartridges() {		
+zip_cartridges() {
 	echo "Compressing the cartridges for upload ..."
 	echo
-	
-	zipfile="build.zip"		
-        gitPath=$(read_conf "git" "path")
-        gitRepo=$(read_conf "git" "repo")
-        gitFile=$(echo "$gitRepo" | rev | cut -d"/" -f1 | rev)
-        repoDir=$(echo "$gitFile" | cut -d"." -f1)
+
+	zipfile="build.zip"
 	cd "$homedir"
 	rm -f build.zip
-	
-	for cartridge in "${cartridges[@]}"; do								
+
+	for cartridge in "${cartridges[@]}"; do
 		echo "${cartridge}"
 		local cartPath=$(scp_cartridge_path "$cartridge")
 		if [ -d "$(echo $cartPath | tr -d '\r')" ]; then
-			cd $repoDir/$gitPath
-			zip -r ../../$zipfile $cartridge -x "*$(scp_exclude "$cartridge")*" -x "*.DS_Store"
-			cd ../..
+			zip -r $zipfile $(echo $cartPath | tr -d '\r') -x "*$(scp_exclude "$cartridge")*" -x "*.DS_Store"
 		fi
 		echo
-	done		
+	done
 }
 
 make_clientcert() {
@@ -53,38 +47,34 @@ make_clientcert() {
 		mkdir "${deploydir}/certs"
 		echo "You need to request a server certificate and password from Demandware and put it in the certs directory!"
 		exit 1
-	else 
+	else
 		echo "Building client certificate."
 	fi
-		
+
 	echo
-	
+
 	demandwareCertificateSRL=$(prompt "deploy" "demandwareCertificateSRL" $string "" "Please enter the name of the server certificate SRL" true "" true)
 	echo
-	
+
 	demandwareCertificateCRT=$(prompt "deploy" "demandwareCertificateCRT" $string "" "Please enter the name of the server certificate CRT" true "" true)
 	echo
-	
+
 	demandwareCertificateKEY=$(prompt "deploy" "demandwareCertificateKEY" $string "" "Please enter the name of the server certificate KEY" true "" true)
 	echo
-	
+
 	demandwareCertificatePassword=$(secure_prompt "deploy" "demandwareCertificatePassword" $string "" "Please enter the password for the server certificate" true "$demandwareCertificateSRL" true)
 	echo
 
 	clientCertificate=$(prompt $demandwareServer "clientCertificate" $string "" "Please enter the client certificate name" true "" true)
 	echo
-	
+
 	clientCertificatePassword=$(prompt $demandwareServer "clientCertificatePassword" $string "" "Please enter the client certificate name" true "$clientCertificate" true)
 	echo
-	
-	echo "a"
-	openssl req -new -newkey rsa:2048 -nodes -out ${deploydir}/certs/$clientCertificate.req -keyout ${deploydir}/certs/$clientCertificate.key -subj "$certSubj" -passout "pass:$clientCertificatePassword"		
-	echo "b"
+
+	openssl req -new -newkey rsa:2048 -nodes -out ${deploydir}/certs/$clientCertificate.req -keyout ${deploydir}/certs/$clientCertificate.key -subj "$certSubj" -passout "pass:$clientCertificatePassword"
 	openssl x509 -CA ${deploydir}/certs/$demandwareCertificateCRT -CAkey ${deploydir}/certs/$demandwareCertificateKEY -CAserial ${deploydir}/certs/$demandwareCertificateSRL -req -in ${deploydir}/certs/$clientCertificate.req -out ${deploydir}/certs/$clientCertificate.pem -days 360 -passin "pass:$demandwareCertificatePassword"
-	echo "c"
 	openssl pkcs12 -export -in ${deploydir}/certs/$clientCertificate.pem -inkey ${deploydir}/certs/$clientCertificate.key -certfile ${deploydir}/certs/$demandwareCertificateCRT -name "$clientCertificate" -out ${deploydir}/certs/$clientCertificate.p12 -passout "pass:$clientCertificatePassword"
-	echo "d"
-	
+
 	write_conf $demandwareServer "clientCertificate" $clientCertificate
 	write_conf_enc $demandwareServer "clientCertificatePassword" $clientCertificatePassword $clientCertificate
 	write_conf "deploy" "demandwareCertificateSRL" $demandwareCertificateSRL
